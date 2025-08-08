a356303113cd472a6a0414b878037455ee9c1065
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2025-08-08 10:16:55 +0200

    logo text updated to be more clear

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return <nav className="fixed top-0 w-full bg-background/80 backdrop-blur-md border-b z-50">
         <div className="container mx-auto px-4 py-4">
             <div className="flex items-center justify-between max-md:justify-center">
-                <div className="text-3xl">KUZMA</div>
+                <div className="text-3xl"><Link href="/">A.KUZMANOVIC</Link></div>
                 <div className="hidden md:flex items-center space-x-8 text-lg uppercase">
                     <Link href="#about"
                           className="text-muted-foreground hover:text-foreground transition-colors">
